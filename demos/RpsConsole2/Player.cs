8351cce6063f89b9253bfea4315abb93dd4200fb
Mark Moore <mark.moore@revature.com>
2022-07-27 20:58:22 -0500

    completes RpsConsole2 with extracted methods and testing

--- demos/RpsConsole2/Player.cs
+++ demos/RpsConsole2/Player.cs
@@ -12,13 +12,34 @@ namespace RpsConsole2
         // an attribute is a single piece of data like an age, DoB, name, etc
         // an access modifier controls what parts of code can access a specific class or field (or property, or method)
         private int myAge = 0;
+        // methods are sets of consecutive steps that the program completes. the can be called indiviudally.
+        //getter and setter methods get/set the data while protecting the data.
+        public int GetAge()
+        {
+            return myAge;
+        }
+
+        public void SetAge(int myAge)
+        {
+            if (myAge > 110 || myAge < 18)
+            {
+                throw new FieldAccessException($"You can't set the age to {myAge}");
+            }
+            else
+            {
+                this.myAge = myAge;
+            }
+        }
 
         // A Property is a special C# abstaction that is a combination of a Getter and a Setter and the data that they get and set.
         public DateTime DoB { get; set; } = new DateTime(1920, 1, 1);
         public string Fname { get; set; }
         public string Lname { get; set; }
         public bool Gender { get; set; }//true == female, false == male
+
         private int wins;
+        //p1.Wins =3;// the runtime will invoke the set method... but you DON'T have to use the value; 
+        //p.wins = wins;
         public int Wins
         {
             get
@@ -27,9 +48,11 @@ namespace RpsConsole2
             }
             set
             {
-                this.wins++;
+                this.wins++;// encapsulation.. the value that i may be trying to give to the property 
+                // is ignored and the current value is incremented by 1.
             }
         }
+
         private int losses;
         public int Losses
         {
@@ -54,26 +77,5 @@ namespace RpsConsole2
             this.Fname = fname;
             this.Lname = lname;
         }
-
-        // methods are sets of consecutive steps that the program completes. the can be called indiviudally.
-        //getter and setter methods get/set the data while protecting the data.
-        public int GetAge()
-        {
-            return myAge;
-        }
-
-        public void SetAge(int myAge)
-        {
-            if (myAge > 110 || myAge < 18)
-            {
-                throw new FieldAccessException($"You can't set the age to {myAge}");
-            }
-            else
-            {
-                this.myAge = myAge;
-            }
-        }
-
-
     }
 }
