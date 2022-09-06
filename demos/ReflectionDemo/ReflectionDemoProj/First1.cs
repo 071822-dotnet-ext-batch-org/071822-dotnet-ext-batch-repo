209bbd45728be1ff5a7190d5c5c503269e160fea
Mark Moore <mark.moore@revature.com>
2022-09-06 12:04:19 -0500

    adds reflection demo

---
+++ demos/ReflectionDemo/ReflectionDemoProj/First1.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReflectionDemoProj
+{
+    public class First1
+    {
+        private int Age { get; set; } = 43;
+        private string Name { get; set; } = "blimey!";
+
+        public First1() { }
+
+        public First1(int age, string name)
+        {
+            this.Age = age;
+            this.Name = name;
+        }
+
+        public int GetMyAge()
+        {
+            return this.Age;
+        }
+
+        public string GetMyName()
+        {
+            return this.Name;
+        }
+
+        private string GetMyNameAndAge()
+        {
+            return $"{this.Name}'s age is {this.Age}";
+        }
+    }
+}
